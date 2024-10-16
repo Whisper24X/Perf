f0a18ba75d0dfe4508998dc50ed49980a69480f4
Whisper <1585485925@qq.com>
2024-10-16 14:06:12 +0800

    windows docker配置

--- mobileperf-master/mobileperf/android/inputfps.py
+++ mobileperf-master/mobileperf/android/inputfps.py
@@ -3,10 +3,20 @@ from datetime import datetime
 import csv
 import os
 import glob
+from mobileperf.android.DB_utils import DatabaseOperations
+from mobileperf.common.log import logger
+
+def get_config_value(file_path, key):
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.strip().startswith(key):
+                return line.split('=')[1].strip()
+    return None
 class FpsListenserImpl(IFpsListener):
     def __init__(self):
-        pass
+        self.package = get_config_value("config.conf", "package")
 
+    @staticmethod
     def get_parent_directory(path, levels=1):
         for _ in range(levels):
             path = os.path.dirname(path)
@@ -24,6 +34,9 @@ class FpsListenserImpl(IFpsListener):
         print(fps_info.jankys_ary)
         print("当前窗口卡顿数(>166.7ms)是：" + str(fps_info.jankys_more_than_166))
         print('\n')
+
+
+
         # 动态获取基路径（假设脚本位于项目的根目录）
         base_path = os.path.dirname(os.path.abspath(__file__))
         # 获取上三级目录路径
@@ -32,7 +45,7 @@ class FpsListenserImpl(IFpsListener):
         source_mobileperf_folder = os.path.join(target_dir)  # 源MobilePerf文件夹路径
 
         target_device_id = devices.replace(':', '_').replace('.', '_')
-        file_path = os.path.join(source_mobileperf_folder, "R", f"_{target_device_id}", "results", f"{str(fps_info.pkg_name)}", "fps_data.csv")
+        file_path = os.path.join(source_mobileperf_folder, "R", f"_{target_device_id}", "results", self.package, "fps_data.csv")
 
 
         #file_path = f"/Users/yangcong/PycharmProjects/Perf/R/_{target_device_id}/results/com.yangcong345.android.phone/fps_data.csv"
@@ -53,3 +66,23 @@ class FpsListenserImpl(IFpsListener):
                 int(fps_info.fps),
                 str(fps_info.jankys_more_than_166)
             ])
+        try:
+            # 数据库连接实例化
+            db_operations = DatabaseOperations()
+
+            # 查询新ids，用于区分新老数据
+            latest_ids = db_operations.get_latest_ids(devices)
+            # Prepare fps_data for insertion into database
+            fps_data = (
+                devices,
+                datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                str(fps_info.pkg_name) + "/" + str(fps_info.window_name),
+                int(fps_info.fps),
+                str(fps_info.jankys_more_than_166),
+                latest_ids
+            )
+            # Insert fps_info into the database
+            db_operations.insert_fpsinfo(fps_data)
+
+        except Exception as db_e:
+            logger.error(f"Failed to insert FPS data into database: {db_e}")
